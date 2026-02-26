825a1c9a4de1831cb74dca4ca8a4cb8bea85e934
Emily <el925766@gmail.com>
2026-02-26 07:43:54 -0500

    El primer ejercicio esta completo, pide tu nombre, tu edad y ciudad, utilice input, int y f-string

---
+++ 01_saludo_formateado.py
@@ -0,0 +1,5 @@
+nombre = input("Hola!, como te llamas?: ")
+edad = int(input("Cual es tu edad?: "))
+ciudad = input("En que ciudad vives?: ")
+
+print(f"Hola {nombre}, tienes {edad} años y viven en {ciudad}")
