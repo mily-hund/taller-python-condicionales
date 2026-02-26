722362bf60ed0bca9cd542f5b34d5afa166b81ba
Emily <el925766@gmail.com>
2026-02-26 07:49:51 -0500

    El segundo ejercicio esta listo, este programa te pide n numero y te muestra si es par o impar, adicionalmente te pregunta tu edad y calcula si eres mayor o menor de edad

---
+++ 02_par_impar.py
@@ -0,0 +1,11 @@
+numero = int(input("Puedes decirme un numero?: "))
+if numero%2 == 0:
+    print("Es un numero par")
+else:
+    print("Es un numero impar")
+
+edad = int(input("Ahora puede decirme tu edad?: "))
+if edad>=18:
+    print("Eres mayor de edad") 
+else:
+    print("Eres menor de edad")
