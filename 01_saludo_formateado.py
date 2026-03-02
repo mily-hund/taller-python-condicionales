dfe229122d9e0d64daccf8f9e7e47ae8db8f18cf
Emily <el925766@gmail.com>
2026-03-01 20:58:32 -0500

    Actualiza 01_saludo_formateado

--- 01_saludo_formateado.py
+++ 01_saludo_formateado.py
@@ -2,4 +2,4 @@ nombre = input("Hola!, como te llamas?: ")
 edad = int(input("Cual es tu edad?: "))
 ciudad = input("En que ciudad vives?: ")
 
-print(f"Hola {nombre}, tienes {edad} años y viven en {ciudad}")
+print(f"Hola {nombre}, tienes {edad} años y vives en {ciudad}")
